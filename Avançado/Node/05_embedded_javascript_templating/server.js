3d4491e501ae9544708090334fef83f0a8152db7
cemgthedev <cemg.the.dev@gmail.com>
2022-11-16 08:50:26 -0300

    for each com EJS

--- Avançado/Node/05_embedded_javascript_templating/server.js
+++ Avançado/Node/05_embedded_javascript_templating/server.js
@@ -5,7 +5,26 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-    res.render("pages/index");
+    const items = [
+        {
+            title: 'D',
+            message: 'Desenvolver aplicações'
+        },
+        {
+            title: 'E',
+            message: 'EJS usa JavaScript'
+        },
+        {
+            title: 'M',
+            message: 'Muito fácil de usar'
+        },
+        {
+            title: 'A',
+            message: 'Amor'
+        }
+    ];
+
+    res.render("pages/index", {qualitys: items,});
 })
 
 app.get("/sobre", function (req, res) {
