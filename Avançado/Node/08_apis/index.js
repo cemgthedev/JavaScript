2c4b273b251937aa07b6775943d0ee7fba0ba48d
cemgthedev <cemg.the.dev@gmail.com>
2023-01-13 20:34:24 -0300

    estudo de body, route e query params

--- Avançado/Node/08_apis/index.js
+++ Avançado/Node/08_apis/index.js
@@ -13,11 +13,21 @@ app.route('/').get((req, res) => res.send(author));
 
 app.route('/').post((req, res) => {res.send(req.body)});
 
+// body params
 app.route('/').put((req, res) => {
-    author = req.body.author;
+    author = req.body.author; 
     res.send(author);
 });
 
+// route params
 app.route('/:identificador').delete((req, res) => {
     res.send(req.params.identificador);
 });
+
+app.route('/identidade/:nome').get((req, res) => {
+    res.send(req.params.nome);
+});
+
+// query params
+// ex: localhost:3000?nome=Mayk&cidade=São Paulo
+app.route('/').get((req, res) => {res.send(req.query.nome)})
