e79e64deb6f4ace0720c05b191aeda08b86ecfcb
cemgthedev <cemg.the.dev@gmail.com>
2023-01-22 18:19:14 -0300

    estudando api no front end com fetch

---
+++ Avançado/06_API_no_front_end_com_featch/app.js
@@ -0,0 +1,44 @@
+// Baixar e executar o projeto: https://github.com/jakeliny/node-api-discover
+const url = "https://localhost:5500/api";
+
+function getUsers() {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => renderAPIResult.textContent = JSON.stringify(data))
+        .catch(error => console.error(error));
+}
+
+// getUsers();
+
+function getUser(userName) {
+    fetch(`${url}/${userName}`)
+        .then(response => response.json())
+        .then(data => {
+            userAvatar.src = data.avatar_url;
+            userRepos.textContent = JSON.stringify(data.repos_url);
+            console.log(btnSearch);
+        })
+        .catch(error => console.error(error));
+}
+
+//getUser('cemgthedev');
+
+const newUser = {
+    avatar_url: "https://picsum.photos/200/300",
+    repos_url: "https://api.github.com/users/maikbrito/repos"
+}
+
+function addUser(newUser) {
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify(newUser),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+        .then(response => response.json())
+        .then(data => apiAlert.textContent = data)
+        .catch(error => console.error(error));
+}
+
+addUser(newUser);
