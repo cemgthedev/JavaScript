bbf00c67b1ec313dd94575b6240b6b7e2322d377
cemgthedev <cemg.the.dev@gmail.com>
2023-01-24 10:33:36 -0300

    iniciando estudo de react e acrescentando tailwindcss como bônus

---
+++ Avançado/08_React/README.md
@@ -0,0 +1,7 @@
+# Estudo de React
+## Criando projeto com Vite
+- npm create vite@latest reactapp --template react
+## Executando
+- cd reactapp
+- npm install
+- npm run dev
---
+++ Avançado/08_React/reactapp/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import '../style/global.css'
+
+export function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div class='bg-slate-700 text-center text-white h-32 justify-center'>
+        <h1 class='text-red-400 text-xl'>Calculadora</h1>
+        <p>{count}</p>
+        <button class='bg-green-700 w-10' onClick={() => setCount((count) => count + 1)}>+</button>
+        <button class='bg-red-700 w-10' onClick={() => setCount((count) => count - 1)}>-</button>
+    </div>
+  )
+}
