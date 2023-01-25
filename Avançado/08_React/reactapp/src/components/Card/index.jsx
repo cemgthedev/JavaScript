2e1e042a882b31f9f2500418b61d132558639306
cemgthedev <cemg.the.dev@gmail.com>
2023-01-25 16:38:07 -0300

    estudando useEffect

--- Avançado/08_React/reactapp/src/components/Card/index.jsx
+++ Avançado/08_React/reactapp/src/components/Card/index.jsx
@@ -1,8 +1,8 @@
 
 export function Card({ name, time }) {
     return (
-        <div class='bg-violet-700 text-white w-[50vw] h-24 m-auto p-2 rounded-md flex justify-between items-center hover:shadow-md hover:shadow-gray-600'>
-            <h1 class='font-semibold'>{ name }</h1>
+        <div className='bg-violet-700 text-white w-[50vw] h-24 m-auto p-2 rounded-md flex justify-between items-center hover:shadow-md hover:shadow-gray-600'>
+            <h1 className='font-semibold'>{ name }</h1>
             <small>{ time }</small>
         </div>
     );
--- Avançado/08_React/reactapp/src/pages/Home/index.jsx
+++ Avançado/08_React/reactapp/src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import '../../style/global.css'
 import {Card} from '../../components/Card'
@@ -23,29 +23,38 @@ export function Home() {
     setPersons(prevState => [...prevState, newPerson]); // ...prevState propaga os elementos do estado anterior dentro do novo array
     // ... se chama spread operator é responsável por propagar os elementos do array
   }
+  
+  /*
+  * Executa sempre que a interface for renderizada ou houver alteração de algum estado
+  * do array de dependências.
+  * Obs: sempre executa uma vez (no carregamento da página)
+  */
+  useEffect(() => {
+      console.log('useEffect foi chamado');
+  }, [persons]);
 
   return (
-    <div class="flex flex-col gap-4 p-4" id="top">
-      <a class="font-bold text-2xl bg-black text-white flex justify-center pt-2 w-10 h-10 fixed right-4 rounded-[100%]" href="#top">
+    <div className="flex flex-col gap-4 p-4" id="top">
+      <a className="font-bold text-2xl bg-black text-white flex justify-center pt-2 w-10 h-10 fixed right-4 rounded-[100%]" href="#top">
         ^
       </a>
-      <header class="flex flex-col p-4 gap-2 mb-0 items-center">
-        <div class="flex items-center justify-between w-[50vw] p-2 rounded-md ring-2 ring-violet-700">
-          <h1 class="font-sans text-3xl text-center">Estudo de React</h1>
-          <div class="flex gap-2 items-center">
-          <h1 class="font-sans text-lg text-center">Carlos Eduardo de Moura Gomes</h1>
-          <img class="w-16 h-16 rounded-[100%]" src="https://github.com/cemgthedev.png" alt="" />
+      <header className="flex flex-col p-4 gap-2 mb-0 items-center">
+        <div className="flex items-center justify-between w-[50vw] p-2 rounded-md ring-2 ring-violet-700">
+          <h1 className="font-sans text-3xl text-center">Estudo de React</h1>
+          <div className="flex gap-2 items-center">
+          <h1 className="font-sans text-lg text-center">Carlos Eduardo de Moura Gomes</h1>
+          <img className="w-16 h-16 rounded-[100%]" src="https://github.com/cemgthedev.png" alt="" />
           </div>
         </div>
-        <h2 class="font-sans w-fit h-8 text-2xl text-center border-b-violet-700 border-b-2">{personName}</h2>
+        <h2 className="font-sans w-fit h-8 text-2xl text-center border-b-violet-700 border-b-2">{personName}</h2>
         <input
-          class="w-[50vw] rounded-md h-10 pl-1 ring-1 ring-slate-100 bg-slate-100 focus:ring-black outline-none"
+          className="w-[50vw] rounded-md h-10 pl-1 ring-1 ring-slate-100 bg-slate-100 focus:ring-black outline-none"
           type="text"
           placeholder='Digite o nome...'
           onChange={e => setPersonName(e.target.value)}
         />
         <button
-          class="w-[50vw] rounded-md ring-1 ring-fuchsia-600 h-10 bg-fuchsia-600 text-white font-bold hover:ring-fuchsia-500 hover:bg-fuchsia-500"
+          className="w-[50vw] rounded-md ring-1 ring-fuchsia-600 h-10 bg-fuchsia-600 text-white font-bold hover:ring-fuchsia-500 hover:bg-fuchsia-500"
           type='button'
           onClick={handleAddPerson}
         >
@@ -53,8 +62,8 @@ export function Home() {
         </button>
       </header>
       <hr/>
-      <main class='flex flex-col gap-4'>
-        <h1 class="font-sans text-2xl text-center">Lista de Presença</h1>
+      <main className='flex flex-col gap-4'>
+        <h1 className="font-sans text-2xl text-center">Lista de Presença</h1>
         {
           persons.map(person => 
             <Card
