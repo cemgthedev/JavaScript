afc350d81bd1acd4d4ca45f2c646baa6a88ae384
cemgthedev <cemg.the.dev@gmail.com>
2023-01-25 08:38:55 -0300

    estudando o uso de propriedades nos componentes

--- Avançado/08_React/reactapp/src/components/Card/index.jsx
+++ Avançado/08_React/reactapp/src/components/Card/index.jsx
@@ -1,8 +1,9 @@
-export function Card() {
+
+export function Card({ name, time }) {
     return (
         <div class='bg-violet-700 text-white w-[50vw] h-24 m-auto p-2 rounded-md flex justify-between items-center hover:shadow-md hover:shadow-gray-600'>
-            <h1 class='font-semibold'>Carlos Eduardo de Moura Gomes</h1>
-            <small>10:15:24</small>
+            <h1 class='font-semibold'>{name}</h1>
+            <small>{time}</small>
         </div>
     );
 }
--- Avançado/08_React/reactapp/src/pages/Home/index.jsx
+++ Avançado/08_React/reactapp/src/pages/Home/index.jsx
@@ -3,27 +3,21 @@ import {Card} from '../../components/Card'
 
 export function Home() {
   return (
-    <div class="flex flex-col gap-2 p-4">
-      <a class="font-bold text-2xl bg-black text-white flex justify-center pt-2 w-10 h-10 fixed right-4 rounded-[100%]" href="#titulo">
+    <div class="flex flex-col gap-4 p-4" id="top">
+      <a class="font-bold text-2xl bg-black text-white flex justify-center pt-2 w-10 h-10 fixed right-4 rounded-[100%]" href="#top">
         ^
       </a>
-      <header class="flex flex-col p-4 gap-2">
-          <h1 class="font-sans text-2xl text-center" id='titulo'>Lista de Presença</h1>
-          <input class="m-auto w-[50vw] h-8 pl-1 ring-2 ring-slate-100 bg-slate-100 focus:ring-fuchsia-300 outline-none" type="text" placeholder='Digite o nome...'/>
+      <header class="flex flex-col p-4 gap-2 mb-0">
+          <h1 class="font-sans text-2xl text-center">Lista de Presença</h1>
+          <input class="m-auto w-[50vw] h-8 pl-1 ring-2 ring-slate-100 bg-slate-100 focus:ring-black outline-none" type="text" placeholder='Digite o nome...'/>
           <button class="m-auto w-[50vw] ring-2 ring-fuchsia-600 h-8 bg-fuchsia-600 text-white font-bold hover:ring-fuchsia-500 hover:bg-fuchsia-500" type='button'>Adicionar</button>
       </header>
-      <hr />
+      <hr/>
       <main class='flex flex-col gap-4'>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+        <Card name="Billy Bombado" time="10:34:56"/>
+        <Card name="João dos Veneno" time="13:32:10"/>
+        <Card name="Paulo Muza" time="15:14:26"/>
+        <Card name="Jully Porca" time="05:22:15"/>
       </main>
     </div>
   )
