6f7fad65425f79ca8d7b04e38e906a3fb0722a66
cemgthedev <cemg.the.dev@gmail.com>
2023-01-25 16:22:40 -0300

    melhorando front end

--- Avançado/08_React/reactapp/src/components/Card/index.jsx
+++ Avançado/08_React/reactapp/src/components/Card/index.jsx
@@ -2,8 +2,8 @@
 export function Card({ name, time }) {
     return (
         <div class='bg-violet-700 text-white w-[50vw] h-24 m-auto p-2 rounded-md flex justify-between items-center hover:shadow-md hover:shadow-gray-600'>
-            <h1 class='font-semibold'>{name}</h1>
-            <small>{time}</small>
+            <h1 class='font-semibold'>{ name }</h1>
+            <small>{ time }</small>
         </div>
     );
 }
--- Avançado/08_React/reactapp/src/pages/Home/index.jsx
+++ Avançado/08_React/reactapp/src/pages/Home/index.jsx
@@ -4,11 +4,13 @@ import '../../style/global.css'
 import {Card} from '../../components/Card'
 
 export function Home() {
+  const [id, setId] = useState(0);
   const [personName, setPersonName] = useState('');
   const [persons, setPersons] = useState([]);
 
   function handleAddPerson() {
     const newPerson = {
+      id: id,
       name: personName,
       time: new Date().toLocaleTimeString('pt-br', {
         hour: '2-digit',
@@ -17,6 +19,7 @@ export function Home() {
       })
     }
     
+    setId(prevState => prevState + 1);
     setPersons(prevState => [...prevState, newPerson]); // ...prevState propaga os elementos do estado anterior dentro do novo array
     // ... se chama spread operator é responsável por propagar os elementos do array
   }
@@ -26,17 +29,23 @@ export function Home() {
       <a class="font-bold text-2xl bg-black text-white flex justify-center pt-2 w-10 h-10 fixed right-4 rounded-[100%]" href="#top">
         ^
       </a>
-      <header class="flex flex-col p-4 gap-3 mb-0">
-        <h1 class="font-sans text-2xl text-center">Lista de Presença</h1>
-        <h2 class="font-sans w-fit h-8 m-auto text-2xl text-center border-b-violet-700 border-b-2">{personName}</h2>
+      <header class="flex flex-col p-4 gap-2 mb-0 items-center">
+        <div class="flex items-center justify-between w-[50vw] p-2 rounded-md ring-2 ring-violet-700">
+          <h1 class="font-sans text-3xl text-center">Estudo de React</h1>
+          <div class="flex gap-2 items-center">
+          <h1 class="font-sans text-lg text-center">Carlos Eduardo de Moura Gomes</h1>
+          <img class="w-16 h-16 rounded-[100%]" src="https://github.com/cemgthedev.png" alt="" />
+          </div>
+        </div>
+        <h2 class="font-sans w-fit h-8 text-2xl text-center border-b-violet-700 border-b-2">{personName}</h2>
         <input
-          class="m-auto w-[50vw] rounded-md h-10 pl-1 ring-1 ring-slate-100 bg-slate-100 focus:ring-black outline-none"
+          class="w-[50vw] rounded-md h-10 pl-1 ring-1 ring-slate-100 bg-slate-100 focus:ring-black outline-none"
           type="text"
           placeholder='Digite o nome...'
           onChange={e => setPersonName(e.target.value)}
         />
         <button
-          class="m-auto w-[50vw] rounded-md ring-1 ring-fuchsia-600 h-10 bg-fuchsia-600 text-white font-bold hover:ring-fuchsia-500 hover:bg-fuchsia-500"
+          class="w-[50vw] rounded-md ring-1 ring-fuchsia-600 h-10 bg-fuchsia-600 text-white font-bold hover:ring-fuchsia-500 hover:bg-fuchsia-500"
           type='button'
           onClick={handleAddPerson}
         >
@@ -45,9 +54,14 @@ export function Home() {
       </header>
       <hr/>
       <main class='flex flex-col gap-4'>
+        <h1 class="font-sans text-2xl text-center">Lista de Presença</h1>
         {
           persons.map(person => 
-            <Card name={ person.name } time={ person.time }/>
+            <Card
+              key={ person.id }
+              name={ person.name }
+              time={ person.time }
+            />
           )
         }
       </main>
