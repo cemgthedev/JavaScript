7c95752a564875851283e717ec1007a3cc795c12
cemgthedev <cemg.the.dev@gmail.com>
2023-02-04 17:45:15 -0300

    consumindo API Fake Store

---
+++ Prática/04 - Consumo de APIs/Fake Store API/index.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.listen('3000');
+console.log('::3000');
+
+app.route('/').get(async (req, res) => {
+    try {
+        const resolve = await axios.get('https://fakestoreapi.com/products');
+        const products = resolve.data;
+        res.send(`
+            ${products.map(product => `
+                <h1>Categoria: ${ product.category }</h1>
+                <h1>Título: ${ product.title }</h1>
+                <img src=${ product.image }/>
+                <h2>Preço: ${ product.price }</h2>
+                <p>Descrição: ${ product.description }</p>
+                <p>Avaliação: ${ product.rating.rate } estrelas ${product.rating.count} avaliações</p>
+            `)}
+        `);
+    } catch(error) {
+        console.error(error);
+    }
+});
